d203adf82321c4a2410c3adc19d96a17d6897f7e
kiyani <bamba.kiyani@gmail.com>
2019-09-16 17:35:39 -0400

    Imported BrowserRouter as Router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router> <App /> </Router>, document.getElementById('root'));
