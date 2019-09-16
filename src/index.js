72f587ca8237d1152ba9bd05622fde7a4089f76b
kiyani <bamba.kiyani@gmail.com>
2019-09-16 17:59:05 -0400

    Created 3 <Route/>

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<Router> <App /> </Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+   document.getElementById('root'));
